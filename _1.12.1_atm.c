9bb33a53374cc166ecd56e772cdd7a55adcbd6dd
saquantum <517393456@qq.com>
2024-09-13 10:35:21 +0100

    Update and rename atm.c to _1.12.1_atm.c

--- _1.12.1_atm.c
+++ _1.12.1_atm.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 
 int main(){
-    int num;
-    int remainder;
-    while(1){
+    int num=1;
+    int remainder=0;
+    while(num%20!=0){ // may as well use do...while
         printf("How much money would you like ? ");
         scanf("%d",&num);
         if (num%20==0){
             printf("OK , dispensing ...");
-            break;
         }else{
             remainder=num%20;
-            printf("I can give you %d or %d , try again .",num-remainder,((num-remainder)/20+1)*20);
+            printf("I can give you %d or %d , try again .\n", \
+            num-remainder,((num-remainder)/20+1)*20);
         }
     }
 }
