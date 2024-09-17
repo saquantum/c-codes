365e449b28ae137c82566b8806b01144d915ca36
saquantum <517393456@qq.com>
2024-09-17 09:33:40 +0100

    Update _2.4.1_leibniz.c

--- _2.4.1_leibniz.c
+++ _2.4.1_leibniz.c
@@ -4,17 +4,17 @@
 #include<time.h>
 
 int main() {
-	double sum = 0;
-	int dn = 1;
-	int sign = 1;
+    double sum = 0;
+    int dn = 1;
+    int sign = 1;
 
-	for (long i = 0; i < 100000000; i++) {
-		sign = dn % 4 == 1 ? +1:-1;
-		sum = sum + 4.0 * (double)sign / (double)dn;
-		dn += 2;
-	}
+    for (long i = 0; i < 100000000; i++) {
+        sign = dn % 4 == 1 ? +1:-1;
+        sum = sum + 4.0 * (double)sign / (double)dn;
+        dn += 2;
+    }
 
-	printf("%.8lf", sum);
-	return 0;
+    printf("%.8lf", sum);
+    return 0;
 
 }
