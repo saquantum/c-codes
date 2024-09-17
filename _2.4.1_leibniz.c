6912847e51a3280437d428162603fea76cb3cd71
saquantum <517393456@qq.com>
2024-09-17 09:35:07 +0100

    Update _2.4.1_leibniz.c

--- _2.4.1_leibniz.c
+++ _2.4.1_leibniz.c
@@ -3,13 +3,15 @@
 #include<assert.h>
 #include<time.h>
 
+#define LOOPS 100000000
+
 int main() {
     double sum = 0;
     // dn stands for denominator
     int dn = 1;
     int sign = 1;
 
-    for (long i = 0; i < 100000000; i++) {
+    for (long i = 0; i < LOOPS; i++) {
         sign = dn % 4 == 1 ? +1:-1;
         sum = sum + 4.0 * (double)sign / (double)dn;
         dn += 2;
