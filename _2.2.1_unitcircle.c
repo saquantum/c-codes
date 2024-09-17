b6677c9ce56820f9a3a1218093a22e290240faba
saquantum <517393456@qq.com>
2024-09-17 09:30:51 +0100

    Update _2.2.1_unitcircle.c

--- _2.2.1_unitcircle.c
+++ _2.2.1_unitcircle.c
@@ -2,16 +2,22 @@
 #include<math.h>
 #include<assert.h>
 
+#define ERR 1e-8
+
 int unitcircle(double x);
+void test();
 
 int main() {
-	
-	assert((unitcircle(0.2) - 1) < 0.00000001);
-	assert((unitcircle(-3) - 1) < 0.00000001);
-	assert((unitcircle(10) - 1) < 0.00000001);
-	assert((unitcircle(100) - 1) < 0.00000001);
+    test();
 }
 
 int unitcircle(double x) {
-	return pow(sin(x), (double)2) + pow(cos(x), (double)2);
+    return pow(sin(x), (double)2) + pow(cos(x), (double)2);
+}
+
+void test(){
+    assert((unitcircle(0.2) - 1) < ERR);
+    assert((unitcircle(-3) - 1) < ERR);
+    assert((unitcircle(10) - 1) < ERR);
+    assert((unitcircle(100) - 1) < ERR);
 }
