e7321446a12480886ac1c95f7abc81cd1e758540
saquantum <517393456@qq.com>
2024-09-22 22:47:35 +0100

    Update _3.4.1_dice.c

--- _3.4.1_dice.c
+++ _3.4.1_dice.c
@@ -9,18 +9,42 @@
 #define TESTS 10000000
 
 void randomthrow(int d[NUMDICE]);
-bool histo_has(const int h[MAXTHROW],const int n);
-void makehist(const int d[NUMDICE],int h[MAXTHROW]);
-bool hists_same(const int h1[MAXTHROW],const int h2[MAXTHROW]);
+bool histo_has(const int h[MAXTHROW], const int n);
+void makehist(const int d[NUMDICE], int h[MAXTHROW]);
+bool hists_same(const int h1[MAXTHROW], const int h2[MAXTHROW]);
 bool isfullhouse(const int d[NUMDICE]);
 bool is4ofakind(const int d[NUMDICE]);
-void test(void);
+void test();
 
+int main() {
+
+	srand(time(NULL));
+	test();
+
+	int dice[NUMDICE];
+	int k4 = 0;
+	int fh = 0;
+	int i;
+
+	for (i = 0; i < TESTS; i++) {
+		randomthrow(dice);
+		if (isfullhouse(dice)) {
+			fh++;
+		}
+		if (is4ofakind(dice)) {
+			k4++;
+		}
+	}
+	printf("FH=%.2f%% 4oK=%.2f%%\n", (double)fh * 100.0 / (double)TESTS, (double)k4 * 100.0 / (double)TESTS);
+
+
+	return 0;
+}
 
 /* Fill the array d with random numbers 1..6 */
 void randomthrow(int d[NUMDICE]) {
-	srand(time(NULL));
-	for (int i = 0; i < NUMDICE; i++) {
+	int i = 0;
+	for (i = 0; i < NUMDICE; i++) {
 		d[i] = 1 + rand() % MAXTHROW;
 	}
 }
@@ -37,8 +61,16 @@ bool histo_has(const int h[MAXTHROW], const int n) {
 
 /* Compute a histogram, given a dice hand */
 void makehist(const int d[NUMDICE], int h[MAXTHROW]) {
-	for (int i = 0; i < NUMDICE; i++) {
-		h[d[i]]++;
+	int i = 0;
+
+	/* since the array is called by reference and we do not
+	   create a new array, thus must first initialise the array */ 
+	for (i = 0; i < MAXTHROW; i++) {
+		h[i] = 0;
+	}
+
+	for (i = 0; i < NUMDICE; i++) {
+		h[d[i]-1]++;
 	}
 }
 
@@ -84,4 +116,64 @@ bool is4ofakind(const int d[NUMDICE]) {
 		}
 	}
 	return flag4;
+}
+
+void test() {
+
+
+	int i, j;
+	int h1[MAXTHROW] = { 0,0,0,0,0,5 };
+	int h2[MAXTHROW] = { 0,0,5,0,0,0 };
+	int h3[MAXTHROW] = { 0,2,0,0,3,0 };
+	int h4[MAXTHROW] = { 2,0,3,0,0,0 };
+	int h5[MAXTHROW] = { 3,2,0,0,0,0 };
+	int h6[MAXTHROW] = { 0,0,0,2,3,0 };
+	int h7[MAXTHROW] = { 4,1,0,0,0,0 };
+	int h8[MAXTHROW] = { 0,0,0,0,1,4 };
+	int h9[MAXTHROW] = { 0,0,0,0,0,0 };
+	int d1[NUMDICE] = { 1,1,1,2,2 };
+	int d2[NUMDICE] = { 5,4,5,4,5 };
+	int d3[NUMDICE] = { 2,1,1,1,1 };
+	int d4[NUMDICE] = { 6,6,6,6,5 };
+	int d5[NUMDICE] = { 6,6,6,6,5 };
+
+	for (i = 0; i < 100; i++) {
+		randomthrow(d5);
+		for (j = 0; j < NUMDICE; j++) {
+			assert(d5[j]>=1 && d5[j]<=MAXTHROW);
+		}
+		makehist(d5, h9);
+		for (j = 0; j < MAXTHROW; j++) {
+			assert(h9[j] >= 0 && h9[j] <= NUMDICE);
+		}
+	}
+	assert(hists_same(h1, h1) == true);
+	assert(hists_same(h5, h5) == true);
+	assert(hists_same(h1, h2) == false);
+	assert(hists_same(h4, h5) == false);
+	makehist(d1, h9); assert(hists_same(h9, h5));
+	makehist(d2, h9); assert(hists_same(h9, h6));
+	makehist(d3, h9); assert(hists_same(h9, h7));
+	makehist(d4, h9); assert(hists_same(h9, h8));
+	assert(histo_has(h1, 5) == true);
+	assert(histo_has(h1, 0) == true);
+	assert(histo_has(h1, 2) == false);
+	assert(histo_has(h2, 5) == true);
+	assert(histo_has(h2, 0) == true);
+	assert(histo_has(h2, 2) == false);
+	assert(histo_has(h3, 3) == true);
+	assert(histo_has(h3, 2) == true);
+	assert(histo_has(h3, 1) == false);
+	assert(histo_has(h4, 3) == true);
+	assert(histo_has(h4, 2) == true);
+	assert(histo_has(h4, 1) == false);
+	assert(isfullhouse(d1) == true);
+	assert(isfullhouse(d2) == true);
+	assert(is4ofakind(d1) == false);
+	assert(is4ofakind(d2) == false);
+	assert(isfullhouse(d3) == false);
+	assert(isfullhouse(d4) == false);
+	assert(is4ofakind(d3) == true);
+	assert(is4ofakind(d4) == true);
+
 }
