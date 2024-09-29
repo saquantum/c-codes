a9934c203416d1eaae513ebabd722a1f2059a3a1
saquantum <517393456@qq.com>
2024-09-29 15:08:58 +0100

    Create _4.7.1_diffusion.c

---
+++ _4.7.1_diffusion.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<assert.h>
+#include<stdbool.h>
+#include<time.h>
+#include<stdlib.h>
+
+#define MAX_ITERATIONS 250
+#define GRID_SIZE 50
+#define X0 GRID_SIZE/2
+#define Y0 GRID_SIZE/2
+#define RED  "\x1B[31m"
+#define NRM  "\x1B[0m"
+
+void random_walk_iteration(bool grid[GRID_SIZE][GRID_SIZE]);
+void random_walk(int* px, int* py);
+bool adjacent(int x, int y, bool grid[GRID_SIZE][GRID_SIZE]);
+
+int main(){
+	srand(time(NULL));
+	bool grid[GRID_SIZE][GRID_SIZE]={0};
+	grid[X0][Y0]=1;
+	
+	for (int i=0;i<MAX_ITERATIONS;i++){
+	    random_walk_iteration(grid);
+	}
+	
+	for(int j=0;j<GRID_SIZE;j++){
+	    for(int i=0;i<GRID_SIZE;i++){
+	        if (grid[i][j]==1) {
+                printf("%s1 ",RED);
+            }
+            else {
+                printf("%s0 ",NRM);
+            }
+	    }
+	    printf("\n");
+	}
+}
+
+void random_walk_iteration(bool grid[GRID_SIZE][GRID_SIZE]){
+    int x0,y0;
+    // find an unoccupied cell
+    while(grid[x0=rand()%GRID_SIZE][y0=rand()%GRID_SIZE]);
+    // and begin random walk
+    while(!adjacent(x0,y0,grid)){
+        random_walk(&x0,&y0);
+    }
+    grid[x0][y0]=1;
+}
+void random_walk(int* px, int* py){
+    int r;
+    switch(r=rand()%4){
+        case 0:
+            *px=*px-1>=0?*px-1:GRID_SIZE-1;
+            break;
+        case 1:
+            *px=*px+1<=GRID_SIZE-1?*px+1:0;
+            break;
+        case 2:
+            *py=*py-1>=0?*py-1:GRID_SIZE-1;
+            break;
+        case 3:
+            *py=*py+1<=GRID_SIZE-1?*py+1:0;
+            break;
+    }
+}
+bool adjacent(int x, int y, bool grid[GRID_SIZE][GRID_SIZE]){
+    bool mid=(grid[x][y]==1);
+    bool left=(grid[x-1>=0?x-1:GRID_SIZE-1][y]==1);
+    bool right=(grid[x+1<=GRID_SIZE-1?x+1:0][y]==1);
+    bool up=(grid[x][y+1<=GRID_SIZE-1?y+1:0]==1);
+    bool down=(grid[x][y-1>=0?y-1:GRID_SIZE-1]==1);
+    return mid || left || right || up || down;
+}
