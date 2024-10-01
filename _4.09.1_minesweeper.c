6bcc6ffb170c859400bee5d5ca908c835a4eb5c8
saquantum <517393456@qq.com>
2024-10-02 00:21:43 +0100

    Update _4.09.1_minesweeper.c

--- _4.09.1_minesweeper.c
+++ _4.09.1_minesweeper.c
@@ -4,20 +4,18 @@
 #include<time.h>
 #include<stdlib.h>
 
-#define GRID_SIZE 10
-#define MINES 20
+#define GRID_SIZE 20
+#define MINES 100
 #define RED  "\x1B[31m"
 #define NRM  "\x1B[0m"
 
 int calcmines(int x, int y, char grid[GRID_SIZE][GRID_SIZE], char c);
 void random_initialise_grid(int mines, char grid[GRID_SIZE][GRID_SIZE]);
-void span0s(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]);
-bool rule1(int known_mines, int x, int y, char board[GRID_SIZE][GRID_SIZE]);
-bool rule2(int* mines, int x, int y, char board[GRID_SIZE][GRID_SIZE]);
+bool rule1(int known_mines, char board[GRID_SIZE][GRID_SIZE]);
+bool rule2(int* mines, char board[GRID_SIZE][GRID_SIZE]);
 void randomclick(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]);
 void weightedclick(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]);
-void onestep(int* known_mines, char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]);
-void span(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]);
+bool span(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]);
 bool win(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]);
 void printboard(char grid[GRID_SIZE][GRID_SIZE]);
 
@@ -25,40 +23,35 @@ int main() {
     char grid[GRID_SIZE][GRID_SIZE] = { 0 };
     srand(time(NULL));
     random_initialise_grid(MINES, grid);
-    //printboard(grid);
+   // printboard(grid);
+
     char board[GRID_SIZE][GRID_SIZE] = { 0 };
 
     int mines = 0;
-    while (!win(grid,board)) {
-        onestep(&mines, grid, board);
-        printf("clicked\n");
-        span(grid, board);
-        printf("span\n");
-        printboard(board);
+    int clicks = 0;
+    while (!win(grid, board)) {
+        weightedclick(grid, board);
+        bool flag0 = false;
+        bool flag1 = false;
+        bool flag2 = false;
+        do{
+            if (flag0 = span(grid, board)) {
+                printboard(board);
+                Sleep(500);
+            }
+            if (flag2 = rule2(&mines, board)) {
+                printboard(board);
+                Sleep(500);
+            }
+            if (flag1 = rule1(mines, board)) {
+                printboard(board);
+                Sleep(500);
+            }
+        } while (flag0 || flag1 || flag2);
     }
     printf("you win!\n");
 }
 
-void onestep(int* known_mines, char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]) {
-    weightedclick(grid, board);
-    //randomclick(grid, board);
-    int count;
-    do {
-        count = 0;
-        for (int j = 0; j < GRID_SIZE; j++) {
-            for (int i = 0; i < GRID_SIZE; i++) {
-                if (rule1(*known_mines, i, j, board)) {
-                    count++;
-                    printf("rule1\n");
-                }
-                if (rule2(known_mines, i, j, board)) {
-                    count++;
-                    printf("rule2\n");
-                }
-            }
-        }
-    } while (count > 0);
-}
 
 void randomclick(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]) {
     int x, y;
@@ -73,17 +66,17 @@ void randomclick(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZ
     else {
         printf("something went wrong.\n");
     }
-    printf("x=%d,y=%d,grid[x][y]=%c\n", x, y, grid[x][y]);
+    //printf("x=%d,y=%d,grid[x][y]=%c\n", x, y, grid[x][y]);
 }
 
 void weightedclick(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]) {
     int x, y;
     int weight = 8 * 8 + 1; // the highest possible weight plus one
-    int temp=0;
+    int temp = 0;
     for (int i = 0; i < GRID_SIZE; i++) {
         for (int j = 0; j < GRID_SIZE; j++) {
             // calc weight
-            if (board[i][j]=='\0') {
+            if (board[i][j] == '\0') {
                 for (int k = -1; k <= 1; k++) {
                     for (int l = -1; l <= 1; l++) {
                         if (!(k == 0 && l == 0) && 0 <= i + k && i + k <= GRID_SIZE - 1 \
@@ -97,12 +90,12 @@ void weightedclick(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_S
                         }
                     }
                 }
-            if (temp < weight) {
-                weight = temp;
-                x = i;
-                y = j;
-            }
-            temp = 0;
+                if (temp < weight) {
+                    weight = temp;
+                    x = i;
+                    y = j;
+                }
+                temp = 0;
             }
         }
     }
@@ -115,54 +108,41 @@ void weightedclick(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_S
         board[x][y] = grid[x][y];
     }
     else {
-        printf("something went wrong.\n");
+        printf("something went wrong. unexpected cell\n");
     }
-    printf("x=%d,y=%d,grid[x][y]=%c,weight=%d\n", x, y, grid[x][y],weight);
+    //printf("x=%d,y=%d,grid[x][y]=%c,weight=%d\n", x, y, grid[x][y], weight);
 }
 
-void span0s(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]) {
+bool rule1(int known_mines, char board[GRID_SIZE][GRID_SIZE]) {
     bool flag = false;
-    for (int i = 0; i < GRID_SIZE; i++) {
-        for (int j = 0; j < GRID_SIZE; j++) {
-            if (board[i][j] == '0') {
-                for (int k = -1; k <= 1; k++) {
-                    for (int l = -1; l <= 1; l++) {
-                        if (!(k == 0 && l == 0) && 0 <= i + k && i + k <= GRID_SIZE - 1 \
-                            && 0 <= j + l && j + l <= GRID_SIZE - 1 && board[i + k][j + l]=='\0') {
-                            board[i + k][j + l] = grid[i + k][j + l];
-                            if (board[i + k][j + l] == '0') {
-                                flag = true;
-                            }
-                        }
-                    }
-                }
+    for (int y = 0; y < GRID_SIZE; y++) {
+        for (int x = 0; x < GRID_SIZE; x++) {
+            if (board[y][x]=='\0' && known_mines == MINES) {
+                board[y][x] = (char)(calcmines(x, y, board, 'x') + '0');
+                flag = true;
             }
         }
     }
-    if (flag) {
-        span0s(grid, board);
-    }
+    return flag;
 }
 
-bool rule1(int known_mines, int x, int y, char board[GRID_SIZE][GRID_SIZE]) {
-    if (known_mines == MINES) {
-        board[x][y] = (char)(calcmines(x, y, board, 'x') + '0');
-        return true;
-    }
-    return false;
-}
-
-bool rule2(int* mines, int x, int y, char board[GRID_SIZE][GRID_SIZE]) {
+bool rule2(int* mines, char board[GRID_SIZE][GRID_SIZE]) {
     int count = 0;
-    int unknown = calcmines(x, y, board, '\0');
-    int known_mines = calcmines(x, y, board, 'x');
-    if (board[x][y] - '0' == unknown + known_mines && unknown > 0) {
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if (!(i == 0 && j == 0) && 0 <= x + i && x + i <= GRID_SIZE - 1 \
-                    && 0 <= y + j && y + j <= GRID_SIZE - 1 && board[x + i][y + j] == '\0') {
-                    board[x + i][y + j] = 'x';
-                    count++;
+    for (int y = 0; y < GRID_SIZE; y++) {
+        for (int x = 0; x < GRID_SIZE; x++) {
+            if ('0' <= board[y][x] && board[y][x] <= '8') {
+                int unknown = calcmines(x, y, board, '\0');
+                int known_mines = calcmines(x, y, board, 'x');
+                if (board[x][y] - '0' == unknown + known_mines && unknown > 0) {
+                    for (int j = -1; j <= 1; j++) {
+                        for (int i = -1; i <= 1; i++) {
+                            if (!(i == 0 && j == 0) && 0 <= x + i && x + i <= GRID_SIZE - 1 \
+                                && 0 <= y + j && y + j <= GRID_SIZE - 1 && board[y + j][x + i] == '\0') {
+                                board[y + j][x + i] = 'x';
+                                count++;
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -171,17 +151,16 @@ bool rule2(int* mines, int x, int y, char board[GRID_SIZE][GRID_SIZE]) {
     return count > 0;
 }
 
-void span(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]) {
+bool span(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]) {
     bool flag = false;
-    for (int i = 0; i < GRID_SIZE; i++) {
-        for (int j = 0; j < GRID_SIZE; j++) {
-            
-            if (board[i][j] == calcmines(i, j, grid, 'x')+'0') {
-                for (int k = -1; k <= 1; k++) {
-                    for (int l = -1; l <= 1; l++) {
-                        if (!(k == 0 && l == 0) && 0 <= i + k && i + k <= GRID_SIZE - 1 \
-                            && 0 <= j + l && j + l <= GRID_SIZE - 1 && board[i + k][j + l] == '\0') {
-                            board[i + k][j + l] = grid[i + k][j + l];
+    for (int y = 0; y < GRID_SIZE; y++) {
+        for (int x = 0; x < GRID_SIZE; x++) {
+            if (board[y][x] == calcmines(x, y, grid, 'x') + '0'){
+                for (int j = -1; j <= 1; j++) {
+                    for (int i = -1; i <= 1; i++) {
+                        if (!(i == 0 && j == 0) && 0 <= x + i && x + i <= GRID_SIZE - 1 \
+                            && 0 <= y + j && y + j <= GRID_SIZE - 1 && board[y + j][x + i] == '\0') {
+                            board[y + j][x + i] = grid[y + j][x + i];
                             flag = true;
                         }
                     }
@@ -189,10 +168,7 @@ void span(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]) {
             }
         }
     }
-    printboard(board);
-    if (flag) {
-        span(grid, board);
-    }
+    return flag;
 }
 
 void random_initialise_grid(int mines, char grid[GRID_SIZE][GRID_SIZE]) {
@@ -200,15 +176,15 @@ void random_initialise_grid(int mines, char grid[GRID_SIZE][GRID_SIZE]) {
     while (mines > 0) {
         x = rand() % GRID_SIZE;
         y = rand() % GRID_SIZE;
-        if (grid[x][y] != 'x') {
-            grid[x][y] = 'x';
+        if (grid[y][x] != 'x') {
+            grid[y][x] = 'x';
             mines--;
         }
     }
     for (int j = 0; j < GRID_SIZE; j++) {
         for (int i = 0; i < GRID_SIZE; i++) {
-            if (grid[i][j] != 'x') {
-                grid[i][j] = calcmines(i, j, grid, 'x') + '0';
+            if (grid[j][i] != 'x') {
+                grid[j][i] = calcmines(i, j, grid, 'x') + '0';
             }
         }
     }
@@ -216,10 +192,10 @@ void random_initialise_grid(int mines, char grid[GRID_SIZE][GRID_SIZE]) {
 
 int calcmines(int x, int y, char grid[GRID_SIZE][GRID_SIZE], char c) {
     int count = 0;
-    for (int i = -1; i <= 1; i++) {
-        for (int j = -1; j <= 1; j++) {
+    for (int j = -1; j <= 1; j++) {
+        for (int i = -1; i <= 1; i++) {
             if (!(i == 0 && j == 0) && 0 <= x + i && x + i <= GRID_SIZE - 1 \
-                && 0 <= y + j && y + j <= GRID_SIZE - 1 && grid[x + i][y + j] == c) {
+                && 0 <= y + j && y + j <= GRID_SIZE - 1 && grid[y+j][x+i] == c) {
                 count++;
             }
         }
@@ -230,7 +206,7 @@ int calcmines(int x, int y, char grid[GRID_SIZE][GRID_SIZE], char c) {
 bool win(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]) {
     for (int j = 0; j < GRID_SIZE; j++) {
         for (int i = 0; i < GRID_SIZE; i++) {
-            if (grid[i][j] != board[i][j]) {
+            if (grid[j][i] != board[j][i]) {
                 return false;
             }
         }
@@ -239,14 +215,17 @@ bool win(char grid[GRID_SIZE][GRID_SIZE], char board[GRID_SIZE][GRID_SIZE]) {
 }
 
 void printboard(char grid[GRID_SIZE][GRID_SIZE]) {
+    printf("\033[H\033[J");
     for (int j = 0; j < GRID_SIZE; j++) {
         for (int i = 0; i < GRID_SIZE; i++) {
-            if (grid[i][j] == 'x') {
+            if (grid[j][i] == 'x') {
                 printf("%s%c ", RED, 'x');
-            }else if (grid[i][j] == '\0') {
-                printf("%s  ", NRM);
-            }else {
-                printf("%s%c ", NRM, grid[i][j]);
+            }
+            else if (grid[j][i] == '\0') {
+                printf("%s. ", NRM);
+            }
+            else {
+                printf("%s%c ", NRM, grid[j][i]);
             }
         }
         printf("%s\n", NRM);
