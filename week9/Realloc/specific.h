c48a29005ac62d134add12fc5d848d01f9e267bf
saquantum <517393456@qq.com>
2024-11-11 14:42:02 +0000

    Create specific.h

---
+++ week9/Realloc/specific.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/*
+A resizeable indexed array ADT. Here:
+1. Setting an element out of bounds reallocs the array to make it valid
+2. Getting an element out-of-bounds is an error
+*/
+
+#define INITSIZE 16
+
+typedef struct{
+    int* a;
+    int size;
+}arr;
+
+/* Creates the empty array */
+arr* arr_init(void);
+/* Similar to l[n] = i, safely resizing if required */
+void arr_set(arr *l, int n, int i);
+/* Similar to = l[n] */
+int arr_get(arr *l, int n);
+/* Clears all space used, and sets pointer to NULL */
+void arr_free(arr **l);
