6abcb51834ea89aa96b12e063b7e3c091365cf6e
saquantum <517393456@qq.com>
2024-11-12 10:34:12 +0000

    Update specific.h

--- week9/_9.01_Realloc/specific.h
+++ week9/_9.01_Realloc/specific.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
+#include "../arr.h"
 /*
 A resizeable indexed array ADT. Here:
 1. Setting an element out of bounds reallocs the array to make it valid
